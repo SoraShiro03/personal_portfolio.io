8ad6249ddf6cd2a646f22c911d1b1d42956ee13a
SkyPH03 <81352625+SoraShiro03@users.noreply.github.com>
2021-11-16 16:49:22 +0630

    add button js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-let flexContainer = document.querySelector(".container")
+let btn = document.querySelector(".btn")
+// let active_0 = document.querySelector(".active--0")
+// let active_1 = document.querySelector(".active--1")
 
-document.querySelector(".btn").addEventListener('click', function(){
-   flexContainer.classList.add(".hidden")
+let container = document.querySelector(".container")
+btn.addEventListener('click', function(){
+   for (i = 1; i < 5; i++){
+      document.querySelector(`.box${i}`).classList.toggle("hidden") && document.querySelector(`.box${i}`).classList.toggle("fadeIn") 
+      
+   }
+   container.classList.toggle("fadeIn")
+   container.classList.toggle("hidden")
+
 })
